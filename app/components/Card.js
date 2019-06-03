14ee4bf6c48c73464fd7237f0647169e7019d963
Gabe <gebizar@uw.edu>
2019-06-03 15:38:29 -0700

    pretty cards working

--- app/components/Card.js
+++ app/components/Card.js
@@ -2,28 +2,35 @@ const React = require('react');
 
 
 
+let CardStyle = ""
 class Card extends React.PureComponent {
 
     constructor(props) {
         super(props)
         this.state = {
-            card: [this.props.suit, this.props.value]
+            len: this.props.len,
+            wid: this.props.wid
         }
         
     }
+
     componentDidMount() {
+        this.setState({
+            suit: this.props.suit,
+            value: this.props.value,
+        })
     }
 
     render() {
-        const { idyll, hasError, updateProps, ...props } = this.props;
+        const { idyll, hasError, updateProps, ...props} = this.props;
         return (
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img src="../static/images/card_back.svg" alt="Avatar" />
+            <div className="flip-card" style={{width: this.state.wid, height: this.state.len}}>
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img className="cardback" src="../static/images/card_back.svg" alt="Avatar" />
                     </div>
-                    <div class="flip-card-back">
-                        <p> {this.state.suit + " " +this.state.value} </p>
+                    <div className="flip-card-back">
+                        <p style={{color: (this.state.suit == "♥" || this.state.suit == "♦")? "#8a0303" : "#000000", "font-size": "3em", "margin-top": (this.state.len / 4)}}> {this.state.suit + this.state.value} </p>
                     </div>
                 </div>
             </div>
--- app/components/CardVis.js
+++ app/components/CardVis.js
@@ -1,20 +1,25 @@
 const React = require('react');
 import styled from 'styled-components';
+import Card from "./Card.js"
 const cardPrimitives = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const suits = ["♠", "♣", "♥", "♦"];
 
 
 
+
 const DeckStyle = styled.div`
-    margin: 0 auto 2rem;
-    padding: 1rem;
-    min-width: 10rem;
-    max-width: 20rem;
+    border-radius: 10%;
+    width: auto !important;
     background-color: white;
     word-wrap: break-word;
     box-shadow: 0 0.0625em 0.1875em 0 rgba(0, 0, 0, 0.1), 0 0.5em 0 -0.25em #f2f2f2, 0 0.5em 0.1875em -0.25em rgba(0, 0, 0, 0.1), 0 1em 0 -0.5em #e5e5e5, 0 1em 0.1875em -0.5em rgba(0, 0, 0, 0.1);
 
 `
+
+// margin: 0 auto 2rem;
+//     padding: 1rem;
+//     min-width: 10rem;
+//     max-width: 20rem;
 
 function makeCards(suits, cardPrimitives) {
     let allPlayedCards = [];
@@ -67,8 +72,7 @@ class CardVis extends React.PureComponent {
         const { idyll, hasError, updateProps, ...props } = this.props;
         return (
           <DeckStyle {...props}>
-            {this.state.allCards[2].suit} 
-            {this.state.allCards[2].value}
+            <Card len={150} wid={100} suit={this.state.allCards[0].suit} value={this.state.allCards[0].value} />
           </DeckStyle>
         );
     }
